741f5530c80454c06dab6760fec2999021502ab8
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-11 21:20:08 +0530

    import java.util.Scanner; public class Test{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         double a = sc.nextDouble();         double b = sc.nextDouble();         System.out.printf("%.2f",a * b);     } }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        double a = sc.nextDouble();
+        double b = sc.nextDouble();
+        System.out.printf("%.2f",a * b);
+    }
+}
