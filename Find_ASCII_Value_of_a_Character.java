90b6b40de43b9de15c6d76fd2899d2dd902dca3d
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-11 21:18:08 +0530

    import java.util.Scanner; public class Test{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         char c = sc.next().charAt(0);         System.out.println((int)c);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        char c = sc.next().charAt(0);
+        System.out.println((int)c);
+    }
+}
