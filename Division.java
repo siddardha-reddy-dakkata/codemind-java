069a8add688829bcdb26828c9f4098400ebac060
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-11 21:23:31 +0530

    import java.util.Scanner; public class Test{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         int a = sc.nextInt();         int b = sc.nextInt();         System.out.println((int) a / b);     } }

---
+++ Division.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.println((int) a / b);
+    }
+}
