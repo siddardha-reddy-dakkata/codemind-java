797f37f30ff7d321383ebcf468c568d8bbb08aef
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-11 22:34:29 +0530

    import java.util.Scanner; public class Test{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         System.out.print((int)(n * (n + 1)) / 2);     } }

---
+++ Find_sum_of_natural_numbers_.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.print((int)(n * (n + 1)) / 2);
+    }
+}
