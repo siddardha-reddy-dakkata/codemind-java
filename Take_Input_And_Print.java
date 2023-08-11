176b838b7a7d3068d21a3642d54adcb529d26cfa
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-11 21:24:34 +0530

    import java.util.Scanner; public class Test{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         String a = sc.nextLine();         System.out.println(a);     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Test{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        System.out.println(a);
+    }
+}
