da2337301c9e87fc27dd7a7474868dafe20a415f
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-08-19 10:36:36 +0530

    import java.util.Scanner; public class Pattern{     public static void main(String[]args){         Scanner sc = new Scanner(System.in);         int n = sc.nextInt();         for(int i = 1;i <= n;i++){             for(int j = 1;j <= n;j++){                 if(j == i){                     System.out.print("0");                 }                 else{                     System.out.print("x");                 }             }             System.out.println();         }     } }

---
+++ print_the_following_pattern_5.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class Pattern{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= n;j++){
+                if(j == i){
+                    System.out.print("0");
+                }
+                else{
+                    System.out.print("x");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
