e214703974f09a2e03b0ea4acce4418288587ef8
siddardha-reddy-dakkata <119839372+siddardha-reddy-dakkata@users.noreply.github.com>
2023-09-01 10:19:22 +0530

    import java.util.Scanner; public class Str{     public static void main(String[]Args){         Scanner sc = new Scanner(System.in);         String s = sc.nextLine();         System.out.println(s.length());     } }

---
+++ characters_in_a_string.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class Str{
+    public static void main(String[]Args){
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+        System.out.println(s.length());
+    }
+}
